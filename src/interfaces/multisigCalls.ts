4d6a72613b9c37fb22ec2306d553ee6e78984553
Furqan <nblogist1@gmail.com>
2021-01-20 21:09:03 +0500

    removed nedb storage

---
+++ src/interfaces/multisigCalls.ts
@@ -0,0 +1,11 @@
+export interface multisig_calls {
+    multisig_address: string,
+    call_hash: string,
+    call_data: string,
+    status: string,
+    approvals: string[],
+    depositor: string,
+    deposit: string,
+    when: any,
+    chain: string
+}
